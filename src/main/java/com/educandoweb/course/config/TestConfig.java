bb8b9c97f626f9e60f2820460b74ac8735db6260
Rafael <rafael.muzzi2@gmail.com>
2022-04-18 01:23:42 -0400

    Category added

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -3,9 +3,11 @@ package com.educandoweb.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -24,8 +26,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category c1 = new Category(null, "Electronics");
+        Category c2 = new Category(null, "Books");
+        Category c3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
         
@@ -34,6 +46,7 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2022-03-04T05:12:07Z"), OrderStatus.WAITING_PAYMENT, u1);
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        
     }
 
 
