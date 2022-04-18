70b3d320e7086215c17c03eba802d91dc82d73ef
Rafael <rafael.muzzi2@gmail.com>
2022-04-18 01:09:52 -0400

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
         
-        Order o1 = new Order(null, Instant.parse("2022-01-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2022-02-02T10:42:07Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2022-03-04T05:12:07Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2022-01-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2022-02-02T10:42:07Z"), OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2022-03-04T05:12:07Z"), OrderStatus.WAITING_PAYMENT, u1);
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
