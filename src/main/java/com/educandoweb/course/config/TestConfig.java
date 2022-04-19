d3edc3950c672ed1ccfe12a355830efb487e446c
Rafael-M1 <rafael.muzzi2@gmail.com>
2022-04-19 11:49:25 -0400

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -37,11 +37,11 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Category c1 = new Category(null, "Electronics");
-        Category c2 = new Category(null, "Books");
-        Category c3 = new Category(null, "Computers");
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
-        categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
         
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
@@ -50,6 +50,16 @@ public class TestConfig implements CommandLineRunner {
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+        
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+        
         
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
