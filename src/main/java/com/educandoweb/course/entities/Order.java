c899d65328bea45a0a6e873b0261e43033ff0e8a
Rafael-M1 <rafael.muzzi2@gmail.com>
2022-04-20 09:33:34 -0400

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -99,6 +99,13 @@ public class Order implements Serializable{
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem x : items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
     public int hashCode() {
         final int prime = 31;
